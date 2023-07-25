5559910603b185bcd12f16d9819b5525cd5204b7
Mohit Patidar <mohitpatidar2186@gmail.com>
2023-07-25 21:36:58 +0530

    modify

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import './App.css';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import About from './components/About';
+import Home  from './components/Home';
 
-function App () {
-   return (
-      <div className="App">
-        <h1>This is iNotebook</h1>
-      </div>
-    );
-
+function App() {
+  return (
+    <section>
+      <Router>
+        <Navbar />
+        <h4 className='container'>This is iNotebook</h4>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </Router>
+    </section>
+  );
 }
 
 export default App;
