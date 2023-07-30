fd379924086e42f5306923e6bfb834708c304ba7
Mohit Patidar <mohitpatidar2186@gmail.com>
2023-07-30 17:24:44 +0530

    modify

--- src/components/Noteitem.js
+++ src/components/Noteitem.js
@@ -1,6 +1,9 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import notesContext from "../context/notes/noteContext"
 
 const Noteitem = (props) => {
+    const context = useContext(notesContext);
+    const { deleteNote } = context;
     const { note } = props;
     return (
         <div className='col-md-3'>
@@ -9,7 +12,7 @@ const Noteitem = (props) => {
                     <h5 class="card-title">{note.title}</h5>
                     <p class="card-text">{note.description}</p>
                     <i class="fa-sharp fa-solid fa-pen-to-square mx-2"></i>
-                    <i class="fa-solid fa-trash mx-2"></i>
+                    <i class="fa-solid fa-trash mx-2" onClick={()=>{deleteNote(note._id)}}></i>
                 </div>
             </div>
         </div>
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -1,130 +1,137 @@
 import NoteContext from "./noteContext";
 import { useState } from "react";
 
-const NoteState = (props)=>{
-    const notesinitial=[
-        {
-          "_id": "64bd3326891f61e5ce2648a7",
-          "user": "64ba65e1aa9f252239756e60",
-          "title": "myasdfasdftitle",
-          "description": "her is asdfasdfdescriptioh ",
-          "tag": "asdf",
-          "date": "2023-07-23T14:03:18.957Z",
-          "__v": 0
-        },
-        {
-          "_id": "64c62d5b7d7267b17fde8eae",
-          "user": "64ba65e1aa9f252239756e60",
-          "title": "wake up ",
-          "description": "ring ring...",
-          "tag": "gym",
-          "date": "2023-07-30T09:28:59.928Z",
-          "__v": 0
-        },
-        {
-          "_id": "64c62d5b7d7267b17fde8eae",
-          "user": "64ba65e1aa9f252239756e60",
-          "title": "wake up ",
-          "description": "ring ring...",
-          "tag": "gym",
-          "date": "2023-07-30T09:28:59.928Z",
-          "__v": 0
-        },
-        {
-          "_id": "64c62d5b7d7267b17fde8eae",
-          "user": "64ba65e1aa9f252239756e60",
-          "title": "wake up ",
-          "description": "ring ring...",
-          "tag": "gym",
-          "date": "2023-07-30T09:28:59.928Z",
-          "__v": 0
-        },
-        {
-          "_id": "64c62d5b7d7267b17fde8eae",
-          "user": "64ba65e1aa9f252239756e60",
-          "title": "wake up ",
-          "description": "ring ring...",
-          "tag": "gym",
-          "date": "2023-07-30T09:28:59.928Z",
-          "__v": 0
-        },
-        {
-          "_id": "64c62d5b7d7267b17fde8eae",
-          "user": "64ba65e1aa9f252239756e60",
-          "title": "wake up ",
-          "description": "ring ring...",
-          "tag": "gym",
-          "date": "2023-07-30T09:28:59.928Z",
-          "__v": 0
-        },
-        {
-          "_id": "64c62d5b7d7267b17fde8eae",
-          "user": "64ba65e1aa9f252239756e60",
-          "title": "wake up ",
-          "description": "ring ring...",
-          "tag": "gym",
-          "date": "2023-07-30T09:28:59.928Z",
-          "__v": 0
-        },
-        {
-          "_id": "64c62d5b7d7267b17fde8eae",
-          "user": "64ba65e1aa9f252239756e60",
-          "title": "wake up ",
-          "description": "ring ring...",
-          "tag": "gym",
-          "date": "2023-07-30T09:28:59.928Z",
-          "__v": 0
-        },
-        {
-          "_id": "64c62d5b7d7267b17fde8eae",
-          "user": "64ba65e1aa9f252239756e60",
-          "title": "wake up ",
-          "description": "ring ring...",
-          "tag": "gym",
-          "date": "2023-07-30T09:28:59.928Z",
-          "__v": 0
-        }
-      ]
+const NoteState = (props) => {
+  const notesinitial = [
+    {
+      "_id": "64bd3326891f61e5ce2648a7",
+      "user": "64ba65e1aa9f252239756e60",
+      "title": "myasdfasdftitle",
+      "description": "her is asdfasdfdescriptioh ",
+      "tag": "asdf",
+      "date": "2023-07-23T14:03:18.957Z",
+      "__v": 0
+    },
+    {
+      "_id": "64c62d5b7d7267b17fde8eae",
+      "user": "64ba65e1aa9f252239756e60",
+      "title": "wake up ",
+      "description": "ring ring...",
+      "tag": "gym",
+      "date": "2023-07-30T09:28:59.928Z",
+      "__v": 0
+    },
+    {
+      "_id": "64c62d5b7d7267b17fde8eae",
+      "user": "64ba65e1aa9f252239756e60",
+      "title": "wake up ",
+      "description": "ring ring...",
+      "tag": "gym",
+      "date": "2023-07-30T09:28:59.928Z",
+      "__v": 0
+    },
+    {
+      "_id": "64c62d5b7d7267b17fde8eae",
+      "user": "64ba65e1aa9f252239756e60",
+      "title": "wake up ",
+      "description": "ring ring...",
+      "tag": "gym",
+      "date": "2023-07-30T09:28:59.928Z",
+      "__v": 0
+    },
+    {
+      "_id": "64c62d5b7d7267b17fde8eae",
+      "user": "64ba65e1aa9f252239756e60",
+      "title": "wake up ",
+      "description": "ring ring...",
+      "tag": "gym",
+      "date": "2023-07-30T09:28:59.928Z",
+      "__v": 0
+    },
+    {
+      "_id": "64c62d5b7d7267b17fde8eae",
+      "user": "64ba65e1aa9f252239756e60",
+      "title": "wake up ",
+      "description": "ring ring...",
+      "tag": "gym",
+      "date": "2023-07-30T09:28:59.928Z",
+      "__v": 0
+    },
+    {
+      "_id": "64c62d5b7d7267b17fde8eae",
+      "user": "64ba65e1aa9f252239756e60",
+      "title": "wake up ",
+      "description": "ring ring...",
+      "tag": "gym",
+      "date": "2023-07-30T09:28:59.928Z",
+      "__v": 0
+    },
+    {
+      "_id": "64c62d5b7d7267b17fde8eae",
+      "user": "64ba65e1aa9f252239756e60",
+      "title": "wake up ",
+      "description": "ring ring...",
+      "tag": "gym",
+      "date": "2023-07-30T09:28:59.928Z",
+      "__v": 0
+    },
+    {
+      "_id": "64c62d5b7d7267b17fde8eae",
+      "user": "64ba65e1aa9f252239756e60",
+      "title": "wake up ",
+      "description": "ring ring...",
+      "tag": "gym",
+      "date": "2023-07-30T09:28:59.928Z",
+      "__v": 0
+    }
+  ]
 
-      const [notes, setNotes] = useState(notesinitial)
+  const [notes, setNotes] = useState(notesinitial)
 
 
-      // Add a note
+  // Add a note
 
-      const addNote = (title , description , tag)=>{
-        const note = {
-          "_id": "64c62d5b7d7267b17fde8eae",
-          "user": "64ba65e1aa9f252239756e60",
-          "title":title,
-          "description": description,
-          "tag": tag,
-          "date": "2023-07-30T09:28:59.928Z",
-          "__v": 0
-        };
-        setNotes(notes.concat(note))
-        
+  const addNote = (title, description, tag) => {
+    const note = {
+      "_id": "64c62d5b7d7267b17fde8eae",
+      "user": "64ba65e1aa9f252239756e60",
+      "title": title,
+      "description": description,
+      "tag": tag,
+      "date": "2023-07-30T09:28:59.928Z",
+      "__v": 0
+    };
+    setNotes(notes.concat(note))
+
+  }
+
+  // delete a note
+
+  const deleteNote = (id) => {
+    const newNotes = notes.filter((note) => {return note._id !== id})
+    setNotes(newNotes)
+  }
+
+  // Edit a note
+
+  const editNote = (id , title , description , tag) => {
+    for (let index = 0; index < notes.length; index++) {
+      const element = notes[index];
+
+      if(element._id  === id){
+        element.title = title;
+        element.description = description;
+        element.tag = tag;
       }
+      
+    }
+  }
 
-      // delete a note
-
-      const deleteNote = (id)=>{
-        const newNotes = notes.filter(()=>{
-          return note._id!==id
-        })
-        setNotes(newNotes)
-      }
-
-      // Edit a note
-
-      const editNote = ()=>{
-        
-      }
-
-    return (
-        <NoteContext.Provider value={{notes , addNote , deleteNote , editNote}}>
-            {props.children}
-        </NoteContext.Provider>
-    )
+  return (
+    <NoteContext.Provider value={{ notes, addNote, deleteNote, editNote }}>
+      {props.children}
+    </NoteContext.Provider>
+  )
 }
 
 export default NoteState;
